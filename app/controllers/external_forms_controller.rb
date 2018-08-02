ecb4262f47cc6c4ecd5933b0d1f2644446b69352
mat <mat@zeilenwerk.ch>
2018-08-02 17:44:03 +0200

    Fix typo.

--- app/controllers/external_forms_controller.rb
+++ app/controllers/external_forms_controller.rb
@@ -2,6 +2,6 @@ class ExternalFormsController < ApplicationController
   skip_authorization_check
 
   def index
-    @url = Rails.env.production? ? "https://glp.puzle.ch/de/externally_submitted_people" : "http://localhost:3000/externally_submitted_people"
+    @url = Rails.env.production? ? "https://glp.puzzle.ch/de/externally_submitted_people" : "http://localhost:3000/externally_submitted_people"
   end
 end
