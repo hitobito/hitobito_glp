b1a3c2ef3e714086c394f89a9901082dfc30b797
mat <mat@zeilenwerk.ch>
2018-08-02 14:47:33 +0200

    Specs passing.

--- app/controllers/externally_submitted_people_controller.rb
+++ app/controllers/externally_submitted_people_controller.rb
@@ -9,23 +9,128 @@ class ExternallySubmittedPeopleController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      person = Person.create(first_name: externally_submitted_person_params[:first_name],
-                             last_name: externally_submitted_person_params[:last_name],
-                             email: externally_submitted_person_params[:email])
-      # person.roles.create(type: "Group::RootZugeordnete::#{externally_submitted_person_params[:role].capitalize}", group: group)
-      Role.create!(type: "Group::RootZugeordnete::#{externally_submitted_person_params[:role].capitalize}", person: person, group: group)
-    end
+      @person = Person.create(first_name: first_name,
+                              last_name: last_name,
+                              email: email)
 
+      if zip_codes_matching_groups.any?
+        zip_codes_matching_groups.each do |group|
+          case submitted_role
+          when "Mitglied", "Sympathisant"
+            if zugeordnete_children(group).any?
+              put_him_into_zugeordnete_children zugeordnete_children(group)
+            else
+              put_him_into_root_zugeordnete_groups
+            end
+          when "Adressverwaltung"
+            if kontakte_children(group).any?
+              put_him_into_kontakte_children kontakte_children(group)
+            else
+              put_him_into_root_kontakte_groups
+            end
+          end
+        end
+      else
+        case submitted_role
+        when "Mitglied", "Sympathisant"
+          put_him_into_root_zugeordnete_groups
+        when "Adressverwaltung"
+          put_him_into_root_kontakte_groups
+        end
+      end
+    end
   end
 
   private
 
-  def group
-    Group.find_by_zip_code(externally_submitted_person_params[:zip_code])
+  def put_him_into_root_zugeordnete_groups
+    root_zugeordnete_groups.each do |group|
+      Role.create(type:   zugeordnete_role_type,
+                  person: @person,
+                  group:  group)
+    end
+  end
+
+  def put_him_into_root_kontakte_groups
+    root_kontakte_groups.each do |group|
+      Role.create!(type:   kontakte_role_type,
+                   person: @person,
+                   group:  group)
+    end
+  end
+
+  def root_zugeordnete_groups
+    Group.where(type: "Group::RootZugeordnete")
+  end
+
+  def root_kontakte_groups
+    Group.where(type: "Group::RootKontakte")
+  end
+
+  def zugeordnete_role_type
+    "Group::RootZugeordnete::#{submitted_role}"
+  end
+
+  def kontakte_role_type
+    "Group::RootKontakte::#{submitted_role}"
+  end
+
+  def zip_codes_matching_groups
+    groups_with_zip_codes = Group.where.not(zip_codes: '')
+    groups_with_zip_codes.select do |group|
+      group.zip_codes.split(",").map(&:strip).include? externally_submitted_person_params[:zip_code]
+    end
+  end
+
+  def zugeordnete_children group
+    if group.children.any?
+      @zugeordnete_children ||= group.children.select{ |child| child.type.include?("Zugeordnete")}
+    else
+      []
+    end
+  end
+
+  def kontakte_children group
+    if group.children.any?
+      @kontakte_children ||= group.children.select{ |child| child.type.include?("Kontakte")}
+    else
+      []
+    end
+  end
+
+  def put_him_into_zugeordnete_children zugeordnete_children
+    zugeordnete_children.each do |zugeordnete_child|
+      Role.create!(type:   "#{zugeordnete_child.type}::#{submitted_role}",
+                   person: @person,
+                   group:  zugeordnete_child)
+    end
+  end
+
+  def put_him_into_kontakte_children kontakte_children
+    kontakte_children.each do |kontakte_child|
+      Role.create!(type:   "#{kontakte_child.type}::#{submitted_role}",
+                   person: @person,
+                   group:  kontakte_child)
+    end
+  end
+
+  def submitted_role
+    externally_submitted_person_params[:role].capitalize
+  end
+
+  def first_name
+    externally_submitted_person_params[:first_name]
+  end
+
+  def last_name
+    externally_submitted_person_params[:last_name]
+  end
+
+  def email
+    externally_submitted_person_params[:email]
   end
 
   def externally_submitted_person_params
     params.require(:externally_submitted_person).permit(:email, :zip_code, :role, :first_name, :last_name)
   end
-
 end
--- spec/controllers/externally_submitted_people_controller_spec.rb
+++ spec/controllers/externally_submitted_people_controller_spec.rb
@@ -2,56 +2,84 @@ require 'spec_helper'
 
 describe ExternallySubmittedPeopleController do
 
-  let!(:group) { groups(:root_zugeordnete) }
+  let!(:root_zugeordnete_group) { groups(:root_zugeordnete) }
+  let!(:root_kontakte_group) { groups(:root_kontakte) }
 
-  context "POST #create" do
-    it "creates a person and saves his/her attributes." do
-      expect do
-        post :create, externally_submitted_person: {zip_code: "3012",
-                                                    email: "sauron@evil.com",
-                                                    first_name: "Sauron",
-                                                    last_name: "The Abominable",
-                                                    role: "mitglied"}, format: :js
-      end.to change{Person.count}.by(1)
-      expect(Person.last.email).to eq "sauron@evil.com"
-      expect(Person.last.first_name).to eq "Sauron"
-      expect(Person.last.last_name).to eq "The Abominable"
-    end
+  let!(:kanton) { groups(:kanton) }
+  let!(:kanton_zugeordnete_group) { groups(:kanton_zugeordnete) }
+  let!(:another_kanton_zugeordnete_group) { groups(:another_kanton_zugeordnete) }
+  let!(:kanton_kontakte_group) { groups(:kanton_kontakte) }
+  let!(:another_kanton_kontakte_group) { groups(:another_kanton_kontakte) }
 
-    it "creates a 'mitglied' role for the new person." do
+  it "creates a person and saves his/her attributes." do
+    expect do
       post :create, externally_submitted_person: {zip_code: "3012",
                                                   email: "sauron@evil.com",
                                                   first_name: "Sauron",
                                                   last_name: "The Abominable",
                                                   role: "mitglied"}, format: :js
-      expect(Role.last.type).to eq "Group::RootZugeordnete::Mitglied"
-    end
+    end.to change{Person.count}.by(1)
+    expect(Person.last.email).to eq "sauron@evil.com"
+    expect(Person.last.first_name).to eq "Sauron"
+    expect(Person.last.last_name).to eq "The Abominable"
+  end
 
-    it "creates a 'sympathisant' role for the new person." do
-      post :create, externally_submitted_person: {zip_code: "3012",
-                                                  email: "sauron@evil.com",
-                                                  first_name: "Sauron",
-                                                  last_name: "The Abominable",
-                                                  role: "sympathisant"}, format: :js
-      expect(Role.last.type).to eq "Group::RootZugeordnete::Sympathisant"
-    end
-
-    it "creates a 'adressverwaltung' role for the new person." do
-      post :create, externally_submitted_person: {zip_code: "3012",
-                                                  email: "sauron@evil.com",
-                                                  first_name: "Sauron",
-                                                  last_name: "The Abominable",
-                                                  role: "adressverwaltung"}, format: :js
-      expect(Role.last.type).to eq "Group::RootZugeordnete::Adressverwaltung"
-    end
-
-    it "puts the newly created person in the proper group." do
-      post :create, externally_submitted_person: {zip_code: "3012",
-                                                  email: "sauron@evil.com",
-                                                  first_name: "Sauron",
-                                                  last_name: "The Abominable",
-                                                  role: "adressverwaltung"}, format: :js
-      expect(Person.last.groups.pluck(:type)).to include "Group::RootZugeordnete"
+  context "when submitted zip code DOES match existing layer groups' zip_codes" do
+    context "it places him in respective subgroups." do
+      it "when submitted role is a mitglied." do
+        post :create, externally_submitted_person: {zip_code: "917 01",
+                                                    email: "sauron@evil.com",
+                                                    first_name: "Sauron",
+                                                    last_name: "The Abominable",
+                                                    role: "mitglied"}, format: :js
+        expect(Person.last.groups).to include kanton_zugeordnete_group, another_kanton_zugeordnete_group
+      end
+      it "when submitted role is a symphatisant" do
+        post :create, externally_submitted_person: {zip_code: "917 01",
+                                                    email: "sauron@evil.com",
+                                                    first_name: "Sauron",
+                                                    last_name: "The Abominable",
+                                                    role: "sympathisant"}, format: :js
+        expect(Person.last.groups).to include kanton_zugeordnete_group, another_kanton_zugeordnete_group
+      end
+      it "when submitted role is a adressverwaltung" do
+        post :create, externally_submitted_person: {zip_code: "917 01",
+                                                    email: "sauron@evil.com",
+                                                    first_name: "Sauron",
+                                                    last_name: "The Abominable",
+                                                    role: "adressverwaltung"}, format: :js
+        expect(Person.last.groups).to include kanton_kontakte_group, another_kanton_kontakte_group
+      end
     end
   end
+
+  context "when submitted zip code DOES NOT match any existing layer groups' zip_codes" do
+    context "it places him in one of root groups equivalents" do
+      it "when submitted role is a mitglied" do
+        post :create, externally_submitted_person: {zip_code: "12345",
+                                                    email: "sauron@evil.com",
+                                                    first_name: "Sauron",
+                                                    last_name: "The Abominable",
+                                                    role: "mitglied"}, format: :js
+        expect(Person.last.groups).to include root_zugeordnete_group
+      end
+      it "when submitted role is a symphatisant" do
+        post :create, externally_submitted_person: {zip_code: "12345",
+                                                    email: "sauron@evil.com",
+                                                    first_name: "Sauron",
+                                                    last_name: "The Abominable",
+                                                    role: "sympathisant"}, format: :js
+        expect(Person.last.groups).to include root_zugeordnete_group
+      end
+      it "when submitted role is a adressverwaltung" do
+        post :create, externally_submitted_person: {zip_code: "12345",
+                                                    email: "sauron@evil.com",
+                                                    first_name: "Sauron",
+                                                    last_name: "The Abominable",
+                                                    role: "adressverwaltung"}, format: :js
+        expect(Person.last.groups).to include root_kontakte_group
+      end
+    end
+  end
+
 end
