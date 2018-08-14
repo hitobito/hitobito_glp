208bae24cc7a808d086f429fe90d32cae9703268
mat <mat@zeilenwerk.ch>
2018-08-14 08:45:24 +0200

    Remove unnecessary roles.

--- app/models/group/root.rb
+++ app/models/group/root.rb
@@ -4,15 +4,6 @@ class Group::Root < Group
 
   children Group::RootGeschaeftsstelle, Group::RootVorstand, Group::RootArbeitsgruppe, Group::RootZugeordnete, Group::RootKontakte, Group::RootGewaehlte, Group::Kanton
 
-  # TODO: Leader and member left in for the time being, otherwise single table inheritance crashes the app.
-  class Leader < Role
-    self.permissions = [:layer_and_below_full, :admin]
-  end
-
-  class Member < Role
-    self.permissions = [:group_read]
-  end
-
   class Administrator < Role
     self.permissions = [:layer_and_below_full, :admin, :impersonation, :contact_data]
   end
