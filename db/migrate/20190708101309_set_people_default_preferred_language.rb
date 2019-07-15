279ed9dad430dbde1f4ee408ca7a1074e8228ca3
Andreas Maierhofer <maierhofer@puzzle.ch>
2019-07-15 10:21:13 +0200

    Fix syntax when setting default preferred language, refs HITO-215

--- db/migrate/20190708101309_set_people_default_preferred_language.rb
+++ db/migrate/20190708101309_set_people_default_preferred_language.rb
@@ -1,10 +1,10 @@
 class SetPeopleDefaultPreferredLanguage < ActiveRecord::Migration
   def up
-    change_column_default(:people, :preferred_language, to: :de)
+    change_column_default(:people, :preferred_language, 'de')
     Person.reset_column_information
   end
 
   def down
-    change_column_default(:people, :preferred_language, to: nil)
+    change_column_default(:people, :preferred_language, nil)
   end
 end
