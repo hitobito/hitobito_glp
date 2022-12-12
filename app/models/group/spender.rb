03b78c5390e011bd4ce055207b07c34acd03b3d3
Nils Rauch <rauch@puzzle.ch>
2022-12-12 09:28:45 +0100

    Skip paper trail for donor role

--- app/models/group/spender.rb
+++ app/models/group/spender.rb
@@ -9,6 +9,9 @@
 class Group::Spender < Group
 
   class Spender < Role
+    # Skips paper_trail for this role type
+    has_paper_trail on: []
+
     self.permissions = []
 
     self.visible_from_above = false
