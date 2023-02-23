2096a47a945e758946b8ee3553b3fa89efe76be6
Matthias Viehweger <viehweger@puzzle.ch>
2023-02-23 13:37:47 +0100

    Merge pull request #40 from hitobito/monitor-sent-mails-better
    Monitor sent mails better

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,52 +1,59 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2019, GLP Schweiz. This file is part of
+#  Copyright (c) 2012-2023, GLP Schweiz. This file is part of
 #  hitobito_glp and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_glp.
 
 
 class Notifier < ApplicationMailer
-  def zip_code_changed person, email
+  def zip_code_changed(person, email)
     @person = person
     mail(to: email)
   end
 
-  def mitglied_left attrs, email
+  def mitglied_left(attrs, email)
     @person = Person.new(attrs)
     mail(to: email)
   end
 
-  def mitglied_joined person, email, jglp
+  def mitglied_joined(person, email, jglp)
     @person = person
     @preferred_language = preferred_language(person)
     @jglp = jglp
-    mail(to: email, subject: "Achtung: Neues Mitglied.")
+    mail(to: email, subject: 'Achtung: Neues Mitglied.')
   end
 
-  def mitglied_joined_monitoring person, submitted_role, email, jglp
+  def mitglied_joined_monitoring(person, submitted_role, email, jglp) # rubocop:disable Metrics/MethodLength it would be better to create 3-4 separate methods out of this
     @person = person
     @category = submitted_role
     @preferred_language = preferred_language(person)
     @jglp = jglp
 
     case submitted_role
-    when "Mitglied"
-      @subject = "Achtung: Neues Mitglied"
-      @welcome = "Ein neues Mitglied hat sich registriert."
-    when "Sympathisant"
-      @subject = "Achtung: Neue/r Sympathisant/in"
-      @welcome = "Ein/e neue/r Sympathisant/in hat sich registriert."
-    when "Medien_und_dritte"
-      @subject = "Achtung: Neue News-Anmeldung"
-      @welcome = "Es gibt eine neue Anmeldung für Partei-News."
+    when 'Mitglied'
+      @subject = 'Achtung: Neues Mitglied'
+      @welcome = 'Ein neues Mitglied hat sich registriert.'
+    when 'Sympathisant'
+      @subject = 'Achtung: Neue/r Sympathisant/in'
+      @welcome = 'Ein/e neue/r Sympathisant/in hat sich registriert.'
+    when 'Medien_und_dritte'
+      @subject = 'Achtung: Neue News-Anmeldung'
+      @welcome = 'Es gibt eine neue Anmeldung für Partei-News.'
       @category = 'Medien & Dritte'
+    else
+      @subject = 'Achtung: Neue Anmeldung'
+      Raven.capture_exception(RuntimeError.new(<<~MESSAGE))
+        Kategorie "#{submitted_role}" wird von "mitglied_joined_monitoring" nicht direkt erwartet.
+
+        Person-ID: #{person.id}
+      MESSAGE
     end
 
     mail(to: email, subject: @subject)
   end
 
-  def welcome_mitglied person, locale
+  def welcome_mitglied(person, locale)
     @person = person
     @locale = locale
     I18n.locale = @locale
@@ -54,21 +61,21 @@ class Notifier < ApplicationMailer
     token = @person.generate_reset_password_token!
     @login_url = edit_person_password_url(reset_password_token: token)
 
-    mail(to: @person.email, subject: t(".subject"))
+    mail(to: @person.email, subject: t('.subject'))
   end
 
-  def welcome_sympathisant person, locale
+  def welcome_sympathisant(person, locale)
     @person = person
     @locale = locale
     I18n.locale = @locale
-    mail(to: @person.email, subject: t(".subject"))
+    mail(to: @person.email, subject: t('.subject'))
   end
 
-  def welcome_medien_und_dritte person, locale
+  def welcome_medien_und_dritte(person, locale)
     @person = person
     @locale = locale
     I18n.locale = @locale
-    mail(to: @person.email, subject: t(".subject"))
+    mail(to: @person.email, subject: t('.subject'))
   end
 
   def preferred_language(person)
