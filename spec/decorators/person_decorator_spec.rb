0896c383cc16e88d071beeb1a6cc162f1bf355e7
Andreas Maierhofer <maierhofer@puzzle.ch>
2023-09-08 08:44:35 +0200

    Spec out that spender roles are shown to spendenverwalter

--- spec/decorators/person_decorator_spec.rb
+++ spec/decorators/person_decorator_spec.rb
@@ -8,24 +8,20 @@
 require 'spec_helper'
 
 describe PersonDecorator do
-
-  let(:admin) { people(:admin) }
   let(:root) { groups(:root) }
-
+  let(:admin) { people(:admin) }
   let(:person) { Fabricate(Group::Root::Administrator.name, group: root).person }
   let(:spender) { Fabricate(Group::Spender.name, parent: groups(:bern)) }
+  let(:decorator) { PersonDecorator.new(person) }
 
-  let(:decorator) { PersonDecorator.new(person.reload) }
-  subject { decorator.roles_grouped }
   before do
     @spender_role = Fabricate(Group::Spender::Spender.name, group: spender, person: person)
+    sign_in(admin)
   end
 
-  context 'as person' do
+  describe 'roles_grouped' do
 
-    before do
-      sign_in(admin)
-    end
+    subject { decorator.roles_grouped }
 
     context :admin do
       it 'includes root but not spender group' do
@@ -61,6 +57,20 @@ describe PersonDecorator do
         expect(subject).to have_key(root)
         expect(subject).not_to have_key(spender)
       end
+    end
+  end
+
+
+  describe 'filtered_roles of other person in spender group' do
+    subject(:filtered_roles) { decorator.filtered_roles(spender) }
+
+    it 'is empty when admin has no spendenverwalter role' do
+      expect(filtered_roles).to eq []
+    end
+
+    it 'is present when admin has spendenverwalter role in group' do
+      Fabricate(Group::Root::Spendenverwalter.name, group: root, person: admin)
+      expect(filtered_roles).to eq [@spender_role]
     end
   end
 end
