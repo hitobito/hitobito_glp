3effe245ee2d9d25257ff484a3e88431e917e226
Matthias Viehweger <viehweger@puzzle.ch>
2023-10-24 13:58:34 +0200

    Bump Version for Release

--- lib/hitobito_glp/version.rb
+++ lib/hitobito_glp/version.rb
@@ -1,11 +1,10 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2019, GLP Schweiz. This file is part of
+#  Copyright (c) 2012-2023, GLP Schweiz. This file is part of
 #  hitobito_glp and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_glp.
 
-
 module HitobitoGlp
-  VERSION = '1.30.8'
+  VERSION = '1.31.0'
 end
